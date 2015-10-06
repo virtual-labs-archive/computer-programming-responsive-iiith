e3d89b46ff3df028670eb7f5c0fdf733682c1df7
Khushpreet kaur <khushpreetk363@gmail.com>
2015-10-06 09:46:57 +0530

    Make 10th experiment available
    - Update 10th experiment with old source code, add to html using applet

---
+++ src/lab/exp10/operator.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+<center>
+<applet code="operator_eval.class" width = "1013" height = "570"  Alt="Java Appelt option is off in your Brouser :( " name = "exp1"  Align = "Middle" >
+</center>
+<param name = "img" value = "fupload">
+</applet>
+
+
+</body>
+</html>
