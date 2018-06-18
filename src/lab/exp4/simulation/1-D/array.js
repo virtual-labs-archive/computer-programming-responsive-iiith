8a7564b8b263b7e524f6bae4e5e0d151504c777b
VishwanathamSarika <vishwanathamsarika@gmail.com>
2018-06-19 02:35:57 +0530

    Fixed the range of input size of array(between 0 and 100000) to avoid alerts of unresponsive pages or longer wait time to load

--- src/lab/exp4/simulation/1-D/array.js
+++ src/lab/exp4/simulation/1-D/array.js
@@ -54,7 +54,7 @@ window.view = {
 	},
 	proceedToStartButton: function() {
 		var userInput = this.getArraySize()
-		if( isNaN( userInput ) === false || userInput < 500000) {
+		if( (isNaN( userInput ) === false) && (userInput < 100000)) {
 			if( userInput !== 0 ) {
 				var element = document.getElementById('inputButtonRadio')
 				element.className = 'show, radioButtonDivision'
@@ -67,7 +67,7 @@ window.view = {
 				alert('Enter array size first !')
 		}
 		else
-			alert( 'Size of the array must be an Integer between 0 and 500000 !' )
+			alert( 'Size of the array must be an Integer between 0 and 100000 !' )
 	},
 	generateRandomNumbers: function() {
 		var inputValue = this.getArraySize()
