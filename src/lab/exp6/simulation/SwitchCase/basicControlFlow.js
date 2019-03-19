92a66ff21e8727bc8c62967426f9d67d5874b700
Animesh Sinha <animeshsinha1309@gmail.com>
2019-03-20 02:10:17 +0530

    Fixing Issues with Float
    Handled cases of floating point entries, avoiding rounding them off and treating them as incorrect entries.

--- src/lab/exp6/simulation/SwitchCase/basicControlFlow.js
+++ src/lab/exp6/simulation/SwitchCase/basicControlFlow.js
@@ -13,7 +13,7 @@ class Initializer {
 	}
 
 	set input(value) { $('#textFieldId').val(value); }
-	get input() { this.inputValue = $('#textFieldId').val(); return parseInt(this.inputValue); }
+	get input() { this.inputValue = $('#textFieldId').val(); return parseFloat(this.inputValue); }
 
 	validate() {
 		if ($('#textFieldId').value === '' || isNaN($('#textFieldId').val())) {
@@ -72,7 +72,7 @@ class Executor {
 		const e31 = [20, 21, 22, 27];
 		const e32 = [20, 21, 23, 24, 27];
 		const e33 = [20, 21, 23, 25, 26, 27];
-		let exec3; if (input === 7) exec3 = e31; else if (input >= 1 && input <= 6) exec3 = e32; else exec3 = e33;
+		let exec3; if (input === 7) exec3 = e31; else if ([1, 2, 3, 4, 5, 6].includes(input)) exec3 = e32; else exec3 = e33;
 		this.execution = exec1.concat(exec2).concat(exec3);
 	}
 
@@ -91,7 +91,7 @@ class Executor {
 		if (lineNumber === 0) {
 			getOutputter().day = this.day;
 		} else if ([4, 6, 8, 10, 12, 14, 16].includes(lineNumber)) {
-			getOutputter().str = getOutputter().week[this.day];
+			getOutputter().str = getOutputter().week[ [ 1, 2, 3, 4, 5, 6, 7 ].includes(this.day) ? this.day : 0];
 		} else if ([22, 24, 26].includes(lineNumber)) {
 			if (lineNumber === 22) getOutputter().out = 'holiday';
 			if (lineNumber === 24) getOutputter().out = 'working';
@@ -121,7 +121,6 @@ class Outputter {
 
 	set day(day) {
 		this.mDay = day;
-		console.assert(day >= 0 && day <= 7, 'Day Index not in Range for Outputter object', this);
 		if (day !== 0) $('#idOfDay').html(this.mDay); else $('#idOfDay').html('');
 	}
 	get day() {
@@ -174,4 +173,4 @@ $(document).ready(function() {
 	getInitializer = function() { return initializer; }
 	getExecutor = function() { return executor; }
 	getOutputter = function() { return outputter; }
-})
+});
