39c3b6c6586570ad18bfb06cb4780160a0db6f50
Dilip Kumar Reddy Kotte <70163818+dilipreddy003@users.noreply.github.com>
2020-09-04 10:21:50 +0530

    mode.inputNumber ===6
    issue 6

--- src/lab/exp6/simulation/SwitchCase/basicControlFlow.js
+++ src/lab/exp6/simulation/SwitchCase/basicControlFlow.js
@@ -221,7 +221,7 @@ window.view = {
 			this.codeExecutionWithColour();
 	 	if (this.currentSiblingElement.className === 'break redClass')
 			this.codeExecutionWithColourAndId('closeBrc1Id');
-		if (1 <= model.inputNumber && model.inputNumber <= 6) {
+		if (1 <= model.inputNumber && model.inputNumber <= 5) {
 	 		if (this.nextSiblingElement.id === 'holidayId')
 				this.codeExecutionWithColourAndId('elseIfId');
 	 		else if (this.nextSiblingElement.id === 'workingdayId') {
@@ -240,7 +240,14 @@ window.view = {
 				this.setInnerHtml('outputDayId', 'INVALID INPUT');
 	 		}
 	 	}
-	 	if (model.inputNumber === 7) {
+	 	if ((model.inputNumber === 7 ) )  {
+	 		if (this.nextSiblingElement.id === 'holidayId') {
+				this.codeExecutionWithColour();
+				this.changeOpacity('holidayImage');
+				this.setInnerHtml('outputDayId', 'HOLIDAY');
+	 		}
+	 	}
+		if ((model.inputNumber === 6 ) )  {
 	 		if (this.nextSiblingElement.id === 'holidayId') {
 				this.codeExecutionWithColour();
 				this.changeOpacity('holidayImage');
