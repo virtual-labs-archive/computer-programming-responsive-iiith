dce4a4ffea8a5d83c6832da67ff8fc69968f0dfc
Dilip Kumar Reddy Kotte <70163818+dilipreddy003@users.noreply.github.com>
2020-09-12 12:12:55 +0530

    changed limits

--- src/lab/exp1/simulation/numericalApproximation.js
+++ src/lab/exp1/simulation/numericalApproximation.js
@@ -290,8 +290,8 @@ window.view = {
 			alert('Enter numeric value of a and b');
 			return false;
 		} 
-		else if (valueA2 >= valueB2 || valueB2 > 30) {
-			alert('Integration Limits are from 0 to 30, b > a and b-a >= 1');
+		else if (valueA2 > 10 || valueA2 < 1 || valueB2 > 10 || valueB2 < 1) {
+			alert('Integration Limits are from 0 to 10');
 			return false;
 		}
 		else {
